5514958714a5a1fdebb10c1dade5682b4f6174bf
Justin <60296568+ComradeDiamond@users.noreply.github.com>
2020-06-05 19:56:50 -0400

    Added Features
    - Animated sidebar for people to select symptoms
    - Added symptom prereqs
    - Fixed up the objects so they work better
    - Added image mapping system where people can click on places to trigger animated sidebar
    - Successfully laid the foundation for the diagnosis display

--- diagnosis.js
+++ diagnosis.js
@@ -1,16 +1,24 @@
 //Symptoms list!
 symptoms = [];
 
-function CreateCondition(name, description, image) {
+function CreateCondition(name, description, image, prerequisite) {
 	//Creates the condition and the associated items with it that we're going to display later
 
 	this.n = name;
 	this.description = description;
 	this.image = image;
+
+	if (prerequisite != undefined)
+	{
+		this.prerequisite = prerequisite;
+	}
+}
+CreateCondition.prototype.checkPrerequisite = function() {
+	return (symptoms.indexOf(this.prerequisite.n) != -1);
 }
 
 //Identifies the treatment type and converts it into text
-const treatmentList = {
+treatmentList = {
 	"boringStuff": new CreateCondition("Revitalize", "Drink 8 ounces of water a day, eat a balanced diet, and get 9 hours of sleep a day.", ""),
 	"quarantine": new CreateCondition("Quarantine", "Stay at home until you feel better.", ""),
 	"convert": new CreateCondition("Convert", "Convert to the light side, and start worshipping the holy faith.", ""),
@@ -49,8 +57,8 @@ const symptomList = {
 	"soreThroat": new CreateCondition("Sore Throat", "A painful feeling in the throat when swallowing", ""),
 
 	//Body
-	"fatigue": new CreateCondition("fatigue", "Overall feeling of tiredness", ""),
-	"tummyAche": new CreateCondition("tummyAche", "Abdominal Pain, might or might not go away", ""),
+	"fatigue": new CreateCondition("Fatigue", "Overall feeling of tiredness", ""),
+	"tummyAche": new CreateCondition("TummyAche", "Abdominal Pain, might or might not go away", ""),
 	"hunger": new CreateCondition("Hunger", "Constantly feeling hungry", ""),
 	"chills": new CreateCondition("Chills", "Feeling irrationally cold", ""),
 	"bleeding": new CreateCondition("General Bleeding", "Any trace of blood in your body", ""),
@@ -64,22 +72,29 @@ const symptomList = {
 	"fingerPain": new CreateCondition("Finger Pain", "Pain when moving your fingers", ""),
 	"veinColor": new CreateCondition("Vein Intensification", "Your veins turn abnormally blue", ""),
 	"rash": new CreateCondition("Rash", "A red color on your arms", ""),
-
-	//Unrevealed
-	"bleedingHand": new CreateCondition("Hand Bleeding", "A sore in your hand, or bleeding in your hands", ""),
-	"bleedingHead": new CreateCondition("Head Bleeding", "Your head is covered in blood", ""),
-
-	"itchingEyes": new CreateCondition("Itching Eyes", "Eyes twitching and you can't resist rubbing them", ""),
-	"itchingHands": new CreateCondition("Itching Hands", "Your hands feel like they are going to go insane", ""),
-
-	"hadMeat": new CreateCondition("Ate Meat or Fish", "You just had meat or fish", ""),
-	"cough": new CreateCondition("Coughed", "Choking on air and then expelling it", ""), //Unlocks via sore throat
-	"troubleBreathing": new CreateCondition("Tight Lungs", "You have trouble breathing", ""),
-	"bonePain": new CreateCondition("Bone Pain", "Your bones hurt no matter what and swell", ""),
-
-	"migrane": new CreateCondition("Migrane", "Extreme sensitivity to light", ""), //Unlocks via dizziness
-	"eyePain": new CreateCondition("Eye Pain","Your eyes hurt when looking at anything light", "")
 }
+
+//Unrevealed items - adding them here because js is getting weird and wants this to initialize before I refer to itself
+symptomList.bleedingHand = new CreateCondition("Hand Bleeding", "A sore in your hand, or bleeding in your hands", "", symptomList.bleeding);
+symptomList.bleedingHead = new CreateCondition("Head Bleeding", "Your head is covered in blood", "", symptomList.bleeding);
+symptomList.itchingEyes = new CreateCondition("Itching Eyes", "Eyes twitching and you can't resist rubbing them", "", symptomList.itching);
+symptomList.itchingHands = new CreateCondition("Itching Hands", "Your hands feel like they are going to go insane", "", symptomList.itching);
+symptomList.hadMeat =  new CreateCondition("Ate Meat or Fish", "You just had meat or fish", "", symptomList.nausea);
+symptomList.cough = new CreateCondition("Coughed", "Choking on air and then expelling it", "", symptomList.soreThroat); //Unlocks via sore throat
+symptomList.troubleBreathing = new CreateCondition("Tight Lungs", "You have trouble breathing", "", symptomList.soreThroat);
+symptomList.bonePain = new CreateCondition("Bone Pain", "Your bones hurt no matter what and swell", "", symptomList.jointPain);
+symptomList.migrane = new CreateCondition("Migrane", "Extreme sensitivity to light", "", symptomList.dizziness); //Unlocks via dizziness
+symptomList.eyePain = new CreateCondition("Eye Pain","Your eyes hurt when looking at anything light","", symptomList.dizziness);
+
+//Here so we can loop add the elements later
+const symptomHead = [symptomList.fever, symptomList.dizziness, symptomList.headaches, symptomList.bleedingHead];
+const symptomEyes = [symptomList.swell, symptomList.eyebags, symptomList.itchingEyes, symptomList.migrane, symptomList.eyePain, symptomList.itchingEyes];
+const symptomMouth = [
+	symptomList.nausea, symptomList.tasteLoss, symptomList.sated, symptomList.soreThroat, symptomList.hadMeat, symptomList.cough, symptomList.troubleBreathing
+];
+const symptomBody = [symptomList.fatigue, symptomList.tummyAche, symptomList.hunger, symptomList.chills, symptomList.bleeding, symptomList.itching];
+const symptomLegs = [symptomList.bonePain, symptomList.jointPain, symptomList.paralysis];
+const symptomArms = [symptomList.itchingHands, symptomList.bleedingHand, symptomList.fingerPain, symptomList.veinColor, symptomList.rash];
 
 function CreateDisease(diseaseName, diseaseSymptoms, diseaseTreatments) { 
 //Creates a new PlagueINC disease that will rampage the world and ruin 2020 even more
@@ -103,25 +118,31 @@ CreateDisease.prototype.relevance = function() {
 
 //Creates the diseases with their official names
 COVID = new CreateDisease("COVID-19", [symptomList.fever.n, symptomList.chills.n, symptomList.fatigue.n, symptomList.troubleBreathing.n, symptomList.veinColor.n] 
-	,[treatmentList.IV.n, treatmentList.hospital.n, treatmentList.pray.n, treatmentList.boringStuff.n, treatmentList.quarantine.n]);
+	,[treatmentList.IV, treatmentList.hospital, treatmentList.pray, treatmentList.boringStuff, treatmentList.quarantine]);
 flu = new CreateDisease("Influenza", [symptomList.fever.n, symptomList.chills.n, symptomList.fatigue.n, symptomList.soreThroat.n], 
-	[treatmentList.boringStuff.n, treatmentList.quarantine.n, treatmentList.ibuprofin.n, treatmentList.vaccine.n]);
-staph = new CreateDisease("Staphylococcus Aureus", [symptomList.rash.n, symptomList.bleedingHand.n, symptomList.itchingHands.n], [treatmentList.cream.n]);
+	[treatmentList.boringStuff, treatmentList.quarantine, treatmentList.ibuprofin, treatmentList.vaccine]);
+staph = new CreateDisease("Staphylococcus Aureus", [symptomList.rash, symptomList.bleedingHand, symptomList.itchingHands], [treatmentList.cream]);
 ecoli = new CreateDisease("Escherichia Coli", [symptomList.nausea.n, symptomList.tummyAche.n, symptomList.hadMeat.n], 
-	[treatmentList.cook.n, treatmentList.asprin.n, treatmentList.hotPillow.n]);
+	[treatmentList.cook, treatmentList.asprin, treatmentList.hotPillow]);
 pneumonia = new CreateDisease("Pneumonia", [symptomList.soreThroat.n, symptomList.fever.n, symptomList.headaches.n, symptomList.cough.n], 
-	[treatmentList.hospital.n, treatmentList.boringStuff.n, treatmentList.IV.n]);
+	[treatmentList.hospital, treatmentList.boringStuff, treatmentList.IV]);
 boneBreak = new CreateDisease("Broken Bones", [symptomList.jointPain.n, symptomList.bonePain.n, symptomList.fingerPain.n], 
-	[treatmentList.surgery.n, treatmentList.hospital.n]);
-concussion = new CreateCondition("Concussion", [symptomList.bleedingHead.n], [treatmentList.surgery.n]);
+	[treatmentList.surgery, treatmentList.hospital]);
+concussion = new CreateCondition("Concussion", [symptomList.bleedingHead.n], [treatmentList.surgery]);
 meNoSleep = new CreateCondition("General Headache", [symptomList.headaches.n, symptomList.dizziness.n, symptomList.swell.n], 
-	[treatmentList.disconnect.n, treatmentList.boringStuff.n, treatmentList.withdrawalCaffiene.n]);
+	[treatmentList.disconnect, treatmentList.boringStuff, treatmentList.withdrawalCaffiene]);
 darkMode = new CreateCondition("Dark Mode User", [symptomList.headaches.n, symptomList.dizziness.n, symptomList.migrane.n, ], 
-	[treatmentList.convert.n, treatmentList.reflecc.n, treatmentList.breakPhone.n]);
+	[treatmentList.convert, treatmentList.reflecc, treatmentList.breakPhone]);
 
 const diseaseList = [COVID, flu, staph, ecoli, pneumonia, boneBreak, concussion, meNoSleep, darkMode];
 
 function interactionCheck() { //Checks to see which thing has the highest percent of match rate
+	if (symptoms == 0)
+	{
+		alert("You're healthy");
+		return;
+	}
+
 	let topGuessPercent = 0;
 	let topGuess = "none";
 
